493af45a4bf9ea8c77f066e17c5753eb0fb667f6
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2020-10-12 15:54:57 +0200

    resolvect: use wrapping for various lists
    dump_list() is used for DNS servers, DNS domains, fallback DNS servers.

--- src/resolve/resolvectl.c
+++ src/resolve/resolvectl.c
@@ -1397,7 +1397,7 @@ static int dump_list(Table *table, const char *prefix, char * const *l) {
 
         r = table_add_many(table,
                            TABLE_STRING, prefix,
-                           TABLE_STRV, l);
+                           TABLE_STRV_WRAPPED, l);
         if (r < 0)
                 return table_log_add_error(r);
 
